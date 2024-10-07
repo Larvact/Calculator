b4fe10c54417737bdd75e2ddd80bdd9d2de1bc40
Larvact <tobyg7794@hotmail.com>
2024-10-07 21:36:15 +0100

    Added result line display to the calculator so we can show results of operations

---
+++ src/main/java/toby/calculator/display/body/pad/expressionpad/ResultLine.java
@@ -0,0 +1,13 @@
+package toby.calculator.display.body.pad.expressionpad;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import lombok.Getter;
+
+@Getter
+public class ResultLine
+{
+    @FXML
+    private TextField expressionResult;
+}
